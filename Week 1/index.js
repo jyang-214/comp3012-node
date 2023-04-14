c4d00b919962cf49b9f583a26c9a4d65cca0dfbd
jyang-214 <jyang214@my.bcit.ca>
2023-04-13 20:32:05 -0700

    Update index.js

--- Week 1/index.js
+++ Week 1/index.js
@@ -1,4 +1,15 @@
-const os = require("os");
+// const os = require("os");    // import 'os' module
 
-const userInfo = os.userInfo();
-console.log(userInfo);
+// const userInfo = os.userInfo();
+// console.log(userInfo);
+
+// const process = require("process");
+// // setTimeout(() => {
+// //     console.log(process.pid);
+// // }, 15000)
+// while(true){
+//     console.log(process.pid);
+// }
+
+const process = require("process");
+process.argv;
