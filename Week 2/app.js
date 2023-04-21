79f8122ba0b694c0f401d1f18a62300798345d42
jyang-214 <jyang214@my.bcit.ca>
2023-04-20 19:02:37 -0700

    Update app.js

--- Week 2/app.js
+++ Week 2/app.js
@@ -1,15 +1,34 @@
+// // Javascript Review
 
-// Function Declaration Syntax
-add(5, 4);      // Hoisting 
-                // Will bring the function declaration to the top of the file
-function add(num1, num2) {
-    return num1 + num2;
-}
+// // Function Declaration Syntax
+// add(5, 4); // Hoisting
+// // Will bring the function declaration to the top of the file
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
 
-// Funciton Expression Syntax
-// No Hoisting
-const add = function(num1, num2) {
-    return num1 + num2;
-}
+// // Funciton Expression Syntax
+// // No Hoisting
+// const add = function (num1, num2) {
+//   return num1 + num2;
+// };
 
-// Arrow Function Syntax
+// // Arrow Function Syntax
+// // No Hoisting
+// const add = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(add(5, 4));
+
+// Indirectly Call the function
+const referenceOne = add;
+const referenceTwo = add;
+console.log(referenceOne(5, 2));
+console.log(referenceTwo(5, 3));
+
+// Primitive Data: Number, String, Boolean, undefined is always going to create a copy
+// Complex Data: Funcitons, Arrays, Objects are always going to create a reference
