ca2f0afe4814c0bf7928f825f950c7d8fc481196
jyang-214 <jyang214@my.bcit.ca>
2023-04-20 18:46:12 -0700

    Update app.js

--- Week 2/app.js
+++ Week 2/app.js
@@ -1,10 +1,15 @@
 
 // Function Declaration Syntax
+add(5, 4);      // Hoisting 
+                // Will bring the function declaration to the top of the file
 function add(num1, num2) {
     return num1 + num2;
 }
 
 // Funciton Expression Syntax
-
+// No Hoisting
+const add = function(num1, num2) {
+    return num1 + num2;
+}
 
 // Arrow Function Syntax
