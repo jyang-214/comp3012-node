cc0c0de7ed458159f69934d930b2c7e5bfca9ee5
jyang-214 <jyang214@my.bcit.ca>
2023-04-20 19:30:19 -0700

    Update app.js

--- Week 2/app.js
+++ Week 2/app.js
@@ -33,7 +33,13 @@ referenceTwo(5, 3);
 // Primitive Data: Number, String, Boolean, undefined is always going to create a copy
 // Complex Data: Funcitons, Arrays, Objects are always going to create a reference
 
-const addTwo = (num1, callback) => {    // addTwo is a Higher Order Function
-    return callback(num1, 2);           // callback is a callback function
-}
+const addTwo = (num1, callback) => {
+  // addTwo is a Higher Order Function
+  return callback(num1, 2); // callback is a callback function
+};
 addTwo(7, add);
+
+// [1, 2, 3, 4].map( (n) => n*5 );
+// ["red", "green"].map( (c) => c.length);
+const arr = [1, 2, 3];
+arr.map(  );
