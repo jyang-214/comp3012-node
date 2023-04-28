804023f3ee0d10dde7e40109a3b73f80efd63cbf
jyang-214 <jyang214@my.bcit.ca>
2023-04-27 19:10:18 -0700

    Week 3

--- Week 3/app.js
+++ Week 3/app.js
@@ -9,3 +9,12 @@
 //                  - Image Processing
 //                  - Video Editing
 //                  - Most functions in the machine learning space
+
+const fs = require("fs");
+fs.writeFile("bcit.txt", "hi", (err) => {
+	console.log("Callback function has been invoked.");
+	if (err) {
+		console.log(err.message);
+	}
+});
+console.log("I was called");
