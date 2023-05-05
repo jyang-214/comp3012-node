56fb1195637a2e7e8562fa5aef9ce95a56b0a1a2
jyang-214 <jyang214@my.bcit.ca>
2023-05-04 18:32:27 -0700

    Week 4

--- Week 4/app.js
+++ Week 4/app.js
@@ -1,11 +1,21 @@
 /** @format */
 
-function calc(n1, n2, cb) {
-	return cb(n1, n2);
-}
+// /** @format */
 
-function add(n1, n2) {
-	return n1 + n2;
-}
+// function calc(n1, n2, cb) {
+// 	return cb(n1, n2);
+// }
 
-console.log(calc(5, 5, add));
+// function add(n1, n2) {
+// 	return n1 + n2;
+// }
+
+// console.log(calc(5, 5, add));
+
+const fs = require("fs");
+const content = fs.readFile("hi.txt", (err, data) => {
+	if (err) {
+		console.log(err);
+	}
+	console.log(data);
+});
