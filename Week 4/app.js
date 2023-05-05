7ca29bfb7f74faa0508b768e287af0e3a4d92992
jyang-214 <jyang214@my.bcit.ca>
2023-05-04 19:04:30 -0700

    Week 4

--- Week 4/app.js
+++ Week 4/app.js
@@ -2,21 +2,53 @@
 
 // /** @format */
 
-// function calc(n1, n2, cb) {
-// 	return cb(n1, n2);
-// }
+// // /** @format */
 
-// function add(n1, n2) {
-// 	return n1 + n2;
-// }
+// // function calc(n1, n2, cb) {
+// // 	return cb(n1, n2);
+// // }
 
-// console.log(calc(5, 5, add));
+// // function add(n1, n2) {
+// // 	return n1 + n2;
+// // }
+
+// // console.log(calc(5, 5, add));
+
+// const fs = require("fs");
+// fs.readFile("hi.txt", "utf-8", (err, data) => {
+// 	if (err) {
+// 		console.log(new Error("Filename doesn't exist!"));
+// 	} else {
+// 		console.log(data);
+// 	}
+// });
 
 const fs = require("fs");
-fs.readFile("hi.txt", "utf-8", (err, data) => {
+fs.readFile("file1.txt", "utf-8", (err, data) => {
 	if (err) {
-		console.log(new Error("Filename doesn't exist!"));
+		return console.log(err);
 	} else {
+		fs.readFile(data, "utf-8", (err, data) => {
+			if (err) {
+				return console.log(err);
+			} else {
+				fs.readFile(data, "utf-8", (err, data) => {
+					if (err) {
+						return console.log(err);
+					} else {
+						fs.readFile(data, "utf-8", (err, data) => {
+							if (err) {
+								return console.log(err);
+							} else {
+								console.log(data);
+							}
+						});
+						console.log(data);
+					}
+				});
+				console.log(data);
+			}
+		});
 		console.log(data);
 	}
 });
