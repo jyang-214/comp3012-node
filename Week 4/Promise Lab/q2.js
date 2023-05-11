d54d035bbaa7118b3daa6526047b090ad2d253eb
jyang-214 <jyang214@my.bcit.ca>
2023-05-11 00:26:35 -0700

    Update q2.js

--- Week 4/Promise Lab/q2.js
+++ Week 4/Promise Lab/q2.js
@@ -1,8 +1,4 @@
-/** @format */
-
 const fs = require("fs");
-
-// Write your Solution Here
 
 fs.readFilePromise = function (fileName) {
 	return new Promise((resolve, reject) => {
@@ -38,25 +34,3 @@ fs.readFilePromise("./file1.txt")
 			.catch((err) => console.log(`Error reading file2: ${err}`));
 	})
 	.catch((err) => console.log(`Error reading file1: ${err}`));
-
-// fs.readFile("file1.txt", (err, fileTwo) => {
-// 	if (err) {
-// 		return console.log(err);
-// 	}
-// 	fs.readFile(fileTwo, (err, fileThree) => {
-// 		if (err) {
-// 			return console.log(err);
-// 		}
-// 		fs.readFile(fileThree, (err, fileFour) => {
-// 			if (err) {
-// 				return console.log(err);
-// 			}
-// 			fs.readFile(fileFour, "utf8", (err, fileFourResult) => {
-// 				if (err) {
-// 					return console.log(err);
-// 				}
-// 				console.log(`Contents of file4: ${fileFourResult}`);
-// 			});
-// 		});
-// 	});
-// });
