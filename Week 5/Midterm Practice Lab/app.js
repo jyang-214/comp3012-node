8cb6ed7d456714db153ed6213392510a3ddb90af
jyang-214 <jyang214@my.bcit.ca>
2023-05-11 20:44:52 -0700

    Update app.js

--- Week 5/Midterm Practice Lab/app.js
+++ Week 5/Midterm Practice Lab/app.js
@@ -2,15 +2,16 @@ const fs = require("fs");
 
 const coffeeTypes = { DR: "dark-roast", MR: "medium-roast", B: "blonde" };
 const viewAllSupply = (coffeeType, callback) => {
-	fs.readFile("supply.txt", "utf-8", (err, data) => {
-		if (err) {
-			callback(err);
-		} else {
-			if (
-				coffeeType === "DR" ||
-				coffeeType === "MR" ||
-				coffeeType === "B"
-			) {
+	if (coffeeType !== "DR" && coffeeType !== "MR" && coffeeType !== "B") {
+		callback(
+			new Error('coffeeType Invalid. Please enter "DR", "MR", or "B"'),
+			null
+		);
+	} else {
+		fs.readFile("supply.txt", "utf-8", (err, data) => {
+			if (err) {
+				callback(err, null);
+			} else {
 				let database = data.split("\n");
 				let counter = 0;
 				database.forEach((element) => {
@@ -19,21 +20,49 @@ const viewAllSupply = (coffeeType, callback) => {
 					}
 				});
 				callback(null, counter);
-			} else {
-				callback(
-					new Error(
-						'coffeeType Invalid. Please enter "DR", "MR", or "B"'
-					)
-				);
 			}
-		}
-	});
+		});
+	}
 };
 
-viewAllSupply("B", (err, data) => {
-	if (err) {
-		console.log(err);
+const addSupply = (coffeeType, callback) => {
+	if (coffeeType !== "DR" && coffeeType !== "MR" && coffeeType !== "B") {
+		callback(
+			new Error('coffeeType Invalid. Please enter "DR", "MR", or "B"'),
+			null
+		);
 	} else {
-		console.log(data);
+		fs.appendFile(
+			"supply.txt",
+			`\n${coffeeTypes[coffeeType]}`,
+			(err, data) => {
+				if (err) {
+					callback(err, null);
+				} else {
+					callback(null, data);
+				}
+			}
+		);
+	}
+};
+
+viewAllSupply("B", (err1, data1) => {
+	if (err1) {
+		console.log(err1);
+	} else {
+		console.log(data1);
+		addSupply("B", (err2, data2) => {
+			if (err2) {
+				console.log(err2);
+			} else {
+				viewAllSupply("B", (err3, data3) => {
+					if (err3) {
+						console.log(err3);
+					} else {
+						console.log(data3);
+					}
+				});
+			}
+		});
 	}
 });
