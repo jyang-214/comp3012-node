97978bca14032f256dfa1dbc072928e63f8d12e7
jyang-214 <jyang214@my.bcit.ca>
2023-05-17 20:14:36 -0700

    Midterm Lab
    Done.

--- Week 5/Midterm Practice Lab/app.js
+++ Week 5/Midterm Practice Lab/app.js
@@ -47,20 +47,77 @@ const addSupply = (coffeeType, callback) => {
 	}
 };
 
-viewAllSupply("B", (err1, data1) => {
+const deleteSupply = (coffeeType, quantity, callback) => {
+	if (coffeeType !== "DR" && coffeeType !== "MR" && coffeeType !== "B") {
+		callback(
+			new Error('coffeeType Invalid. Please enter "DR", "MR", or "B"'),
+			null
+		);
+	} else {
+		let database = null;
+		fs.readFile("supply.txt", "utf-8", (err, data) => {
+			if (err) {
+				callback(err, null);
+			} else {
+				database = data.split(EOL);
+				if (quantity === "*") {
+					quantity = database.length;
+				}
+				for (let i = 0; i < quantity; i++) {
+					const index = database.indexOf(coffeeTypes[coffeeType]);
+
+					if (index > -1) {
+						database.splice(index, 1);
+					}
+				}
+				const joinedDatabase = database.join("\n");
+				fs.writeFile("supply.txt", joinedDatabase, (err, data) => {
+					if (err) {
+						callback(err, null);
+					} else {
+						callback(null, data);
+					}
+				});
+			}
+		});
+	}
+};
+
+const coffee = "B";
+const quantity = 5;
+
+viewAllSupply(coffee, (err1, data1) => {
 	if (err1) {
 		console.log(err1);
 	} else {
-		console.log(data1);
-		addSupply("B", (err2, data2) => {
+		console.log(`All Supplies for ${coffeeTypes[coffee]} is ${data1}`);
+		addSupply(coffee, (err2, data2) => {
 			if (err2) {
 				console.log(err2);
 			} else {
-				viewAllSupply("B", (err3, data3) => {
+				viewAllSupply(coffee, (err3, data3) => {
 					if (err3) {
 						console.log(err3);
 					} else {
-						console.log(data3);
+						console.log(
+							`All Supplies for ${coffeeTypes[coffee]} is ${data3}`
+						);
+						deleteSupply(coffee, quantity, (err4, data4) => {
+							if (err4) {
+								console.log(err4);
+							} else {
+								viewAllSupply(coffee, (err5, data5) => {
+									if (err5) {
+										console.log(err);
+									} else {
+										console.log(
+											`All Supplies for ${coffeeTypes[coffee]} is ${data5}`
+										);
+										console.log(`Program is completed.`);
+									}
+								});
+							}
+						});
 					}
 				});
 			}
